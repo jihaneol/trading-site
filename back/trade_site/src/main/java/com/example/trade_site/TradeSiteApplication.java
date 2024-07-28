5cf34edb468ca7dbc4bbadafb0eb9d67a976e198
jihaneol <6531z@naver.com>
2024-07-28 18:17:17 +0900

    create: 백엔드 파일 생성

---
+++ back/trade_site/src/main/java/com/example/trade_site/TradeSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.trade_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSiteApplication.class, args);
+	}
+
+}
---
+++ back/trade_site/src/test/java/com/example/trade_site/TradeSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trade_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
